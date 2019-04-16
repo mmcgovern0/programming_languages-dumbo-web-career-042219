b2918485b0c3f288027ed408c4ba5057a7a55945
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 13:40:42 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,6 +5,6 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |type, hash|
     hash.each do |name, attribute|
-      new_hash[name] ||
+      new_hash[name] ||= attribute
     
 end
